3699f307221d7d612a60fe083d9ef92e5a8401d4
mano <manoranjan.kumar.dev@gmail.com>
2026-01-30 05:24:12 +0530

    refactor: extracts delimiter parsing and validation

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -4,33 +4,52 @@ class StringCalculator {
       return 0;
     }
 
-    RegExp delimiter = RegExp('[,\n]');
+    final delimiter = _extractDelimiter(numberList);
+    final numbers = _removeDelimiterHeader(numberList);
 
-    if (numberList.startsWith('//')) {
-      delimiter = RegExp(numberList[2]);
-      numberList = numberList.substring(4);
+    final parts = numbers.split(delimiter);
+    final negatives = <int>[];
+
+    final sum = _sumValues(parts, negatives);
+
+    _throwIfNegativesExist(negatives);
+
+    return sum;
+  }
+
+  RegExp _extractDelimiter(String input) {
+    if (input.startsWith('//')) {
+      return RegExp(input[2]);
     }
+    return RegExp('[,\n]');
+  }
 
-    final List<String> splittedParts = numberList.split(delimiter);
+  String _removeDelimiterHeader(String input) {
+    if (input.startsWith('//')) {
+      return input.substring(4);
+    }
+    return input;
+  }
+
+  int _sumValues(List<String> parts, List<int> negatives) {
     int sum = 0;
-    final List<int> negativeNumberList = [];
 
-    for (String part in splittedParts) {
-      int parsedValue = int.parse(part);
+    for (final part in parts) {
+      final value = int.parse(part);
 
-      if (parsedValue < 0) {
-        negativeNumberList.add(parsedValue);
+      if (value < 0) {
+        negatives.add(value);
       }
 
-      sum += parsedValue;
-    }
-
-    if (negativeNumberList.isNotEmpty) {
-      throw Exception(
-        'negative numbers not allowed ${negativeNumberList.join(',')}',
-      );
+      sum += value;
     }
 
     return sum;
+  }
+
+  void _throwIfNegativesExist(List<int> negatives) {
+    if (negatives.isNotEmpty) {
+      throw Exception('negative numbers not allowed ${negatives.join(',')}');
+    }
   }
 }
