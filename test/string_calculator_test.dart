207df65537b3301e5e6504c4736ce058ee88549f
mano <manoranjan.kumar.dev@gmail.com>
2026-01-30 04:08:06 +0530

    test: initialize StringCalculator class

---
+++ test/string_calculator_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:string_calculator_tdd/string_calculator.dart';
+
+void main() {
+  test('initialize StringCalculator class', () {
+    final StringCalculator calculator = StringCalculator();
+  });
+}
